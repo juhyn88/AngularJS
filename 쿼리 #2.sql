18b526b61e3738e0fb288909ee8aa29af714fad6
juhyn88 <68534373+juhyn88@users.noreply.github.com>
2021-03-11 15:28:10 +0900

    Delete 쿼리 #2.sql

--- 쿼리 #2.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_board_bjh(
-   no INT(11)
-   ,name VARCHAR(50)
-   ,email VARCHAR(50)
-   ,gender VARCHAR(20)
-   ,title VARCHAR(100)
-   ,content VARCHAR(1000)
-   ,PRIMARY KEY(NO)
-);
